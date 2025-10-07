a713b631e8786152e1130a58897ae60926966cb3
Hitesh Choudhary <hk2310756@gmail.com>
2025-10-07 17:48:28 +0530

    RTE again api change

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Editor } from '@tinymce/tinymce-react';
-import { forwardRef } from 'react';
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { forwardRef } from "react";
 
-
-const RTE = forwardRef(function RTE({ value = "", onChange, onInit },ref) { 
+const RTE = forwardRef(function RTE({ value = "", onChange, onInit }, ref) {
   return (
     <div>
       <Editor
         ref={ref}
-        apiKey='no-api-key'
+        apiKey="ux5b8lrrmgvffm4wah6hhb60yeqtrjzh8chfq1h5iwq92mag"
         initialValue={value}
         onInit={(evt, editor) => onInit && onInit(editor)}
         onEditorChange={(content) => onChange && onChange(content)}
@@ -16,19 +15,36 @@ const RTE = forwardRef(function RTE({ value = "", onChange, onInit },ref) {
           height: 300,
           menubar: true,
           plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
           ],
-          toolbar: 'undo redo | blocks | ' +
-            'bold italic forecolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          toolbar:
+            "undo redo | blocks | " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         }}
       />
     </div>
   );
 });
 
-export default RTE
+export default RTE;
